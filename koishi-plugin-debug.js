3c0fe3b3409ac4be0cd5e680565dc91b5af5b5e0
GSCSDSUN <46085443+GSCSDSUN@users.noreply.github.com>
2021-09-01 19:38:12 +0800

    Update koishi-plugin-debug.js

--- koishi-plugin-debug.js
+++ koishi-plugin-debug.js
@@ -18,3 +18,5 @@ function unescape(ctx,text){
     console.log(segment.unescape(text));
     return segment.unescape(text);
 }
+
+vmess://eyJhZGQiOiI0Ny4yNDIuNTkuMjI4IiwiYWlkIjoiMCIsImhvc3QiOiIiLCJpZCI6IjE4MGY4ZDc5LWVjYzQtNDc3Yi1iZTg3LWRlODMxYzk1MjhlOSIsIm5ldCI6IndzIiwicGF0aCI6IiIsInBvcnQiOiIxMTQ1MiIsInBzIjoiaXBfNDcuMjQyLjU5LjIyOCIsInNjeSI6ImF1dG8iLCJzbmkiOiIiLCJ0bHMiOiIiLCJ0eXBlIjoiIiwidiI6IjIifQ==
